5cf7e2c658dcfbf5541a27b64c0229fed37d990a
Ali Bayramli <bayramliali@tutanota.com>
2022-02-21 23:32:13 +0100

    User form setup

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*#?&^_-]{8,}$/)
+  ]);
+  confirm_password = new FormControl('', [
+    Validators.required,
+  ]);
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13),
+  ]);
+  showAlert = false;
+  alertMessage = 'Please wait! Your account is being created.';
+  alertColor = 'blue';
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber,
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  register() {
+    this.showAlert = true;
+    this.alertMessage = 'Please wait! Your account is being created.';
+    this.alertColor = 'blue';
   }
-
 }
