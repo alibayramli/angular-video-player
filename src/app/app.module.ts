5f386454d2f1a2323adb52ce707218b776ee2d94
Ali Bayramli <bayramliali@tutanota.com>
2022-05-29 17:43:17 +0200

    Add routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,14 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from 'src/environments/environment';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    HomeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
