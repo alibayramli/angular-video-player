640b55a9ff907fbf154ca9135808b42471337c55
Ali Bayramli <bayramliali@tutanota.com>
2022-05-29 18:33:24 +0200

    Refactor auth logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
 import { delay, map, Observable } from 'rxjs';
 import IUser from '../models/user.model';
 
@@ -15,6 +16,7 @@ export class AuthService {
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore,
+    private router: Router,
   ) {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(
@@ -46,6 +48,16 @@ export class AuthService {
 
     await userCred.user.updateProfile({
       displayName: userData.name,
-    })
+    });
+  }
+
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+
+    await this.auth.signOut();
+
+    await this.router.navigateByUrl('/');
   }
 }
