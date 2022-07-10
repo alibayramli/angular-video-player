96c3d0019f4f72854113fd4fdc38548c0d79437b
Ali Bayramli <bayramliali@tutanota.com>
2022-07-10 10:42:48 +0200

    Bind edit form

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import IClip from 'src/app/models/clip.model';
 import { ModalService } from 'src/app/services/modal.service';
 
@@ -7,16 +8,32 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, OnDestroy {
+export class EditComponent implements OnInit, OnChanges, OnDestroy {
   @Input() activeClip: IClip | null = null;
+
+  clipID = new FormControl('');
+  title = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID,
+  })
   constructor(private modal: ModalService) { }
 
   ngOnInit(): void {
     this.modal.register('editClip');
   }
 
+  ngOnChanges(): void {
+    if (!this.activeClip) {
+      return;
+    }
+    this.clipID.setValue(this.activeClip.docID);
+    this.title.setValue(this.activeClip.title);
+  }
   ngOnDestroy(): void {
     this.modal.unRegister('editClip');
   }
-
 }
