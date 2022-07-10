564018f2d9161c9a5d3a072b5b58693124dfffa1
Ali Bayramli <bayramliali@tutanota.com>
2022-07-10 10:29:14 +0200

    Bind active clip when selected

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, Input } from '@angular/core';
+import IClip from 'src/app/models/clip.model';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit, OnDestroy {
-
+  @Input() activeClip: IClip | null = null;
   constructor(private modal: ModalService) { }
 
   ngOnInit(): void {
