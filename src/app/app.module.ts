7d0dc5c065950a497fa4c4e3cc3357eb60dd138c
Ali Bayramli <bayramliali@tutanota.com>
2022-08-30 17:37:11 +0200

    Load videos to home page - inifinite scrolling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutComponent } from './about/about.component';
 import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ClipsListComponent } from './clips-list/clips-list.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +23,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AboutComponent,
     ClipComponent,
     NotFoundComponent,
+    ClipsListComponent,
   ],
   imports: [
     BrowserModule,
